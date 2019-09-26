0bb8e83f2946639aebc8edd078440f7104ef8cf4
ffahim <ferdousj2ee@gmail.com>
2019-09-26 23:15:50 +0600

    upto UserDetails

--- src/main/java/com/example/polls/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/polls/security/JwtAuthenticationEntryPoint.java
@@ -13,15 +13,15 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JwtAuthenticationEntryPoint  implements AuthenticationEntryPoint {
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 	private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
-    @Override
-    public void commence(HttpServletRequest httpServletRequest,
-                         HttpServletResponse httpServletResponse,
-                         AuthenticationException e) throws IOException, ServletException {
-        logger.error("Responding with unauthorized error. Message - {}", e.getMessage());
-        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
-    }
+
+	@Override
+	public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+			AuthenticationException e) throws IOException, ServletException {
+		logger.error("Responding with unauthorized error. Message - {}", e.getMessage());
+		httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+	}
 
 }
